642e9fc00d62b2a5a3e4a284990b3e788c455d6a
SHW378 <cesarbrmn@hotmail.com>
2025-03-06 12:31:09 -0600

    1.1

--- PracticaSP.sql
+++ PracticaSP.sql
@@ -23,3 +23,61 @@
 	--		No existe el registro
 	--		insert
 
+create database Tiendita
+GO
+use Tiendita
+GO
+
+CREATE TABLE Producto (
+	Id int primary key identity(1,1) not null,
+    Producto varchar(250),
+	Cantidad int, 
+	PrecioUnitario decimal(18,2),
+	Categoria varchar(50),
+
+)
+go
+create procedure  sp_DeTiendita
+(
+@Producto varchar(250),
+@Categoria varchar(250),
+@Cantidad int,
+@PrecioUnitario decimal(10, 2),
+@IdVentaSalida int output,
+@Mensaje varchar(250) output
+)
+as
+BEGIN
+if @Cantidad <= 0 or @PrecioUnitario <= 0
+begin
+set @Mensaje = 'La cantidad y el precio unitario deben ser mayores a 0.'
+END
+else 
+BEGIN
+if exists (select 1 from Ventas where Producto = @Producto and Categoria = @Categoria)
+begin
+update Ventas
+set Cantidad = Cantidad + @Cantidad
+where Producto = @Producto and Categoria = @Categoria
+select @IdVentaSalida = ID from Ventas where Producto = @Producto and Categoria = @Categoria
+set @Mensaje = 'Venta actualizada.'
+end
+else 
+BEGIN
+insert into Ventas (Producto, Categoria, Cantidad, PrecioUnitario)
+values (@Producto, @Categoria, @Cantidad, @PrecioUnitario)
+set @IdVentaSalida = scope_identity()
+set @Mensaje = 'Venta insertada.'
+END
+END
+END
+go
+
+
+declare @IdVentaSalida int
+declare @Mensaje varchar(250)
+
+exec sp_DeTiendita 'Laptop', 'Electrónica', 5, 1500.00, @IdVentaSalida output, @Mensaje output
+
+print 'ID de la venta: ' + cast(@IdVentaSalida as varchar)
+print 'Mensaje: ' + @Mensaje
