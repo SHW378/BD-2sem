09d66058f11b20099a92844d8e8f1d07a749ef4c
SHW378 <cesarbrmn@hotmail.com>
2025-03-07 16:07:07 -0600

    Parcial1

---
+++ Parcial1.sql
@@ -0,0 +1,15 @@
+/*
+gbd_Parcial1_82196 
+ID con identity(1,1)
+Tablas: 
+-- Empleados 
+	- ID, Nombre, Telefono
+- Producto 
+	- Id, Nombre, Marca, Precio, Existencias
+- Estimaciones
+	- ID, Producto, Empleados, Precio total
+- Ventas
+	- ID, Fecha, Empleado, Cliente, Producto
+- Clientes
+	- Id, Nombre, Direcciones, Telefono, correo
+*/
