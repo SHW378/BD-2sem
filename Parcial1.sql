132fd8e47a227c57c75065ca13ec92f525d9dd40
SHW378 <cesarbrmn@hotmail.com>
2025-03-08 19:42:58 -0600

    parcialparte1

--- Parcial1.sql
+++ Parcial1.sql
@@ -13,3 +13,47 @@ Tablas:
 - Clientes
 	- Id, Nombre, Direcciones, Telefono, correo
 */
+
+create database gbd_Parcial1_82196
+go
+use gbd_Parcial1_82196
+go
+
+create table Empleados (
+ID int primary key identity(1,1) not null,
+Nombre_Empl varchar(250),
+Telefono varchar(250)
+)
+go
+
+create table Producto (
+ID int primary key identity(1,1) not null,
+Nombre_Prod varchar(250),
+Marca varchar(250),
+Precio int,
+Existencias int
+)
+go
+
+create table Estimaciones (
+ID int primary key identity(1,1) not null,
+PrecioTotal int,
+ProductoID int foreign key references Producto(ID),
+EmpleadoID int foreign key references Empleados(ID)
+)
+
+create table Clientes (
+ID int primary key identity(1,1) not null,
+Nombre_Cliente varchar(250),
+Direcciones varchar(250),
+Telefono varchar(250),
+Correo varchar(250)
+)
+
+create table Ventas (
+ID  int primary key identity(1,1) not null,
+Fecha datetime default getdate(),
+EmpleadoID int foreign key references Empleados(ID),
+ClienteID int foreign key references Clientes(ID),
+ProductoID int foreign key references Producto(ID)
+)
